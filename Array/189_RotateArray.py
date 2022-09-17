e80a265c07f90976d19a4e2192a8d28321914290
aubrey-zyx <aubrey0930@163.com>
2022-09-17 22:57:28 +0800

    Upload

---
+++ Array/189_RotateArray.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+
+        def reverse(start, end):
+            nums[start: end + 1] = nums[start: end + 1][::-1]
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
+
+'''
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        tmp = nums.copy()
+        for i in range(n):
+            tmp[(i + k) % n] = nums[i]
+        nums[:] = tmp
+'''
