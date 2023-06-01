e72be1582c1a1e68b151ac1da78e601c13fbd847
aubrey-zyx <aubrey0930@163.com>
2023-06-01 14:44:34 +0800

    Upload

--- Array/442_FindDuplicatesInArray.py
+++ Array/442_FindDuplicatesInArray.py
@@ -9,3 +9,20 @@ class Solution:
                 ans.append(abs(num))
             nums[abs(num) - 1] *= -1
         return ans
+
+
+class Solution2:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            while nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+                '''
+                "nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]" does not work
+                
+                better:
+                p=nums[i]
+                nums[i], nums[p - 1] = nums[p - 1], nums[i]
+                
+                https://stackoverflow.com/questions/68152730/understand-python-swapping-why-is-a-b-b-a-not-always-equivalent-to-b-a-a
+                '''
+        return [num for i, num in enumerate(nums) if num - 1 != i]
