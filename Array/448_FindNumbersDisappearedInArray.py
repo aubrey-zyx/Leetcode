18e06883994527cb2fe3929e99dc13ac695709af
aubrey-zyx <aubrey0930@163.com>
2023-06-05 23:15:59 +0800

    Upload

--- Array/448_FindNumbersDisappearedInArray.py
+++ Array/448_FindNumbersDisappearedInArray.py
@@ -10,3 +10,21 @@ class Solution:
             if i not in nums_set:
                 ans.append(i)
         return ans
+
+
+class Solution2:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            while nums[i] != nums[nums[i] - 1]:
+                p = nums[i]
+                nums[i], nums[p - 1] = nums[p - 1], nums[i]
+        return [i + 1 for i, num in enumerate(nums) if num - 1 != i]
+
+
+class Solution3:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for num in nums:
+            x = (num - 1) % n
+            nums[x] += n
+        return [i + 1 for i, num in enumerate(nums) if num <= n]
