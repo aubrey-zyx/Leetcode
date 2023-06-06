c50527df89c7e1bcfd4090afe066a1e144b0bce0
aubrey-zyx <aubrey0930@163.com>
2023-06-07 00:49:40 +0800

    Upload

--- Array/189_RotateArray.py
+++ Array/189_RotateArray.py
@@ -16,8 +16,8 @@ class Solution:
         reverse(0, k - 1)
         reverse(k, n - 1)
 
-'''
-class Solution:
+
+class Solution2:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -27,4 +27,3 @@ class Solution:
         for i in range(n):
             tmp[(i + k) % n] = nums[i]
         nums[:] = tmp
-'''
