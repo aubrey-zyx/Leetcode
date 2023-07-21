ee56d41d0def48da769d87fc849c90a616d533f6
Yaxuan Zheng <108720789+aubrey-zyx@users.noreply.github.com>
2023-07-22 00:36:28 +0800

    Upload

--- LinkedList/21_MergeTwoSortedLists.py
+++ LinkedList/21_MergeTwoSortedLists.py
@@ -16,3 +16,15 @@ class Solution:
             cur = cur.next
         cur.next = list1 if list1 else list2
         return prehead.next
+
+
+class Solution2:    # recursively
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not (list1 and list2):
+            return list1 if list1 else list2
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2
