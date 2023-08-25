78a7b32383fbcc6ebaca199de4e046ac0174d055
Yaxuan Zheng <108720789+aubrey-zyx@users.noreply.github.com>
2023-08-24 23:18:26 -0400

    Upload

---
+++ Tree/94_BinaryTreeInorderTraversal.py
@@ -0,0 +1,30 @@
+# Recursively
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def inorder(root):
+            nonlocal ans
+            if not root:
+                return None
+            inorder(root.left)
+            ans.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return ans
+
+# Iteratively
+class Solution2:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        stack = []
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            ans.append(cur.val)
+            cur = cur.right
+        return ans
