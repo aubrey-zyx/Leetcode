61512fbdf57c1d1f3f9bf1a714cfa3cbbf7e9912
Yaxuan Zheng <108720789+aubrey-zyx@users.noreply.github.com>
2023-12-29 23:32:34 -0800

    Upload

--- Trie/1268_SearchSuggestionsSystem.py
+++ Trie/1268_SearchSuggestionsSystem.py
@@ -24,26 +24,38 @@ class Solution:
 
 
 # Trie
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.words = []
+
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        res = []
-        ht = collections.defaultdict(list)
-        for prod in products:
-            i = 0
-            while i < len(prod) and i < len(searchWord):
-                if prod[i] != searchWord[i]:
-                    break
-                i += 1
-                ht[prod[:i]].append(prod)
-        for val in ht.values():
-            val.sort()
+        def insert(root, word):
+            cur = root
+            for c in word:
+                if c not in c.children:
+                    cur.children[c] = TrieNode()
+                cur = cur.children[c]
+                cur.words.append(word)
+                cur.words.sort()
+                if len(cur.words) > 3:
+                    cur.words.pop()
 
-        for i in range(len(searchWord)):
-            matches = ht[searchWord[: i + 1]]
-            if len(matches) > 3:
-                res.append(matches[: 3])
+        root = TrieNode()
+        for prod in products:
+            insert(root, prod)
+
+        res = []
+        cur = root
+        flag = False
+        for c in searchWord:
+            if flag or c not in cur.children:
+                res.append([])
+                flag = True
             else:
-                res.append(matches)
-            i += 1
+                cur = cur.children[c]
+                res.append(cur.words)
 
         return res
