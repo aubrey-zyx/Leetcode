48d7140394b6a3e86ec685cde394383fb8923f9a
Aubrey Zheng <yaxuanzh@andrew.cmu.edu>
2024-09-21 22:42:20 -0400

    Upload

--- Java Solutions/49_GroupAnagrams.java
+++ Java Solutions/49_GroupAnagrams.java
@@ -13,3 +13,28 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+
+class Solution2 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            int[] counter = new int[26];
+            for (char ch : word.toCharArray()) {
+                counter[ch - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if (counter[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(counter[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(word);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
